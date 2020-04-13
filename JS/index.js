b1a68e42ec299435694464224313036003a7a62c
PlankCipher <3abqarino3@gmail.com>
2020-04-13 16:12:06 -0700

    Detect current section on load and close the nav menu on clicking a link.

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     "use strict";
 
+    changeCurrent();
+
     var togglerBtn = $("nav i");
     togglerBtn.click(function () {
         if (togglerBtn.hasClass("fa-bars")) {
@@ -14,24 +16,36 @@ $(document).ready(function () {
     });
 
     $(window).on("scroll", function () {
+        changeCurrent();
+    });
+
+    function changeCurrent() {
         var links = $("nav ul li").children("a");
 
         var stop = Math.round($(window).scrollTop());
 
         for (var link of links) {
             $(link).removeClass("current");
-            var scrollToOffset = $("#" + $(link).attr("data-scroll-to")).offset().top;
+            if ($("#" + $(link).attr("data-scroll-to")).length > 0) {
+                var scrollToOffset = $("#" + $(link).attr("data-scroll-to")).offset().top;
 
-            if (stop + ($(window).height() / 2) > scrollToOffset) {
-                $(link).addClass("current");
-                break;
+                if (stop + ($(window).height() / 2) > scrollToOffset) {
+                    $(link).addClass("current");
+                    break;
+                }
             }
         }
-    });
+    }
 
     $("nav ul li a").on("click", function () {
+        var navBarHeight = $("nav").height();
+
         $("html, body").animate({
-            scrollTop: $("#" + $(this).attr("data-scroll-to")).offset().top
-        }, 1500);
+            scrollTop: $("#" + $(this).attr("data-scroll-to")).offset().top - navBarHeight
+        }, 1000);
+
+        $("nav i").removeClass("fa-times");
+        $("nav i").addClass("fa-bars");
+        $("nav ul").removeClass("displayed");
     });
 });
