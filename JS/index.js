c4304a99b57b31d866f8dfc6a50969b91232a22e
PlankCipher <3abqarino3@gmail.com>
2020-04-15 09:50:09 -0700

    Fix section detection upon scrolling (marks more than one link as current).

--- JS/index.js
+++ JS/index.js
@@ -21,11 +21,15 @@ $(document).ready(function () {
 
     function changeCurrent() {
         var links = $("nav ul li").children("a");
+        links = Array.prototype.reverse.call(links);
 
         var stop = Math.round($(window).scrollTop());
 
         for (var link of links) {
             $(link).removeClass("current");
+        }
+
+        for (var link of links) {
             if ($("#" + $(link).attr("data-scroll-to")).length > 0) {
                 var scrollToOffset = $("#" + $(link).attr("data-scroll-to")).offset().top;
 
